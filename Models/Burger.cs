0875fa09925e8c033719aa19e5e6ace6df02e4bc
ColeyDohmen <coley.dawn12@gmail.com>
2021-04-07 09:37:30 -0600

    edit model

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -2,11 +2,13 @@ namespace burgers.Models
 {
     public class Burger
     {
-        public Burger(string cheese)
+        public Burger(string name, string cheese)
         {
+            Name = name;
             Cheese = cheese;
         }
 
         public string Cheese { get; set; }
+        public string Name { get; set; }
     }
 }
